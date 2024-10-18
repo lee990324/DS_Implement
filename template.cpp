87bfb82887a670c021928126357cbf9b40902248
mintWaterMelon <134702226+lee990324@users.noreply.github.com>
2024-10-17 18:22:31 -0700

    Modified

--- template.cpp
+++ template.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
-
+/*
+// function template
 template <typename T>
 T add(T x, T y) {
 	return x + y;
@@ -14,9 +15,33 @@ int main ()
 	double num3 = 10.52, num4 = 20.24;
 	cout << add(num3, num4)	<< endl;
 }
+*/
+// class template
+template <class T>
+class Calcu
+{
+	private:
+		T num1;
+		T num2;
+	public:
+		Calcu(T num1, T num2) {
+			this->num1 = num1;
+			this->num2 = num2;
+		}
+		T GetAdd() {
+			return num1 + num2;
+		}
+};
 
+int main ()
+{
+	Calcu<int> calcu1(10, 20);
+	cout << calcu1.GetAdd() << endl;
 
-
+	
+	Calcu<double> calcu2(10.52, 20.24);
+	cout << calcu2.GetAdd() << endl;
+}	
 
 /*
 template <class T>
