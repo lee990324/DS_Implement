301a42877952bc486312f0836d1d0057f3c14ac2
mintWaterMelon <134702226+lee990324@users.noreply.github.com>
2024-10-17 21:15:12 -0700

    Modified

--- stack.cpp
+++ stack.cpp
@@ -1,4 +1,6 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 template<class T>
@@ -6,7 +8,7 @@ class Stack
 {
 	public:
 		Stack(int stackCapacity = 10);
-		~stack() {delete [] stack;}
+		~Stack() {delete [] stack;}
 		bool IsEmpty() const;
 		T& Top() const;
 		void Push(const T& item);
@@ -15,13 +17,64 @@ class Stack
 		T *stack;
 		int top;
 		int capacity;
-}
+};
 
 template<class T>
-Stack<T>::stack(int stackCapacity)
-		:capacity(stackCapacity)
+Stack<T>::Stack(int stackCapacity):capacity(stackCapacity)
 {
 	if (capacity < 1)
 		throw "Stack capacity must be > 0";
-		
+	stack = new T[capacity];
+	top = -1;	
 }
+template<class T>
+inline bool Stack<T>::IsEmpty() const
+	{return top == -1;}
+
+template<class T>
+inline T& Stack<T>::Top() const
+{
+	if (IsEmpty())
+		throw "Stack is empty";
+	return stack[top];
+}
+template<class T>
+void Stack<T>::Push(const T& x)
+{
+	if (top == capacity - 1)
+	       throw "Full!";
+	stack[++top] = x;
+}
+template <class T>
+void Stack<T>::Pop()
+{
+	if (IsEmpty())
+		throw "Stack is empty. Cannot delete.";
+	stack[top--].~T();
+}
+
+
+int main()
+{
+	Stack<int> stack(5);
+	stack.Push(5);
+	stack.Push(3);	
+	stack.Push(5);
+	stack.Push(4);	
+	cout << stack.Top() << endl;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
